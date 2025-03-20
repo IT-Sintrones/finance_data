a2ca6c5f6e14002aedb2eaa2733881fcba4e3e53
JessicaWeng-Sintrones <jessicaweng@sintrones.com>
2025-03-20 16:17:07 +0800

    Update

--- Parse Finance Data.py
+++ Parse Finance Data.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import requests
 from bs4 import BeautifulSoup
